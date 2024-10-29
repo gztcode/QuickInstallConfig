0c5ac2debaab7a5d6044a99d3c4364a0590d096e
gzt <gztcode@gmail.com>
2024-10-29 17:31:45 +0800

    Add files via upload

--- install_mysql.sh
+++ install_mysql.sh
@@ -25,14 +25,16 @@ yum install -y mysql-community-server
 # mysqld --initialize --user=mysql --datadir=$DATA_DIR
 
 # 启动 MySQL
+echo "MySQL 启动中..."
 systemctl start mysqld
 systemctl enable mysqld
 
 # 获取临时密码
 TEMP_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-
+echo "获取临时密码:${TEMP_PASSWORD} 并修改为新的密码:${PASSWORD}"
 # 修改密码
-mysql -u root -p "$TEMP_PASSWORD" -e "set global validate_password.policy=0;set global validate_password.length=4;ALTER USER 'root'@'localhost' IDENTIFIED BY '$PASSWORD';FLUSH PRIVILEGES;"
+mysql --connect-expired-password -u root -p"$TEMP_PASSWORD" -e "alter user 'root'@'localhost' identified by 'Abcd1234@'";
+mysql -u root -p"Abcd1234@" -e "set global validate_password.policy=0;set global validate_password.length=4;ALTER USER 'root'@'localhost' IDENTIFIED BY '$PASSWORD';FLUSH PRIVILEGES;"
 
 # 修改配置文件设置端口并将监听地址从 localhost 改为 0.0.0.0
 sed -i "s/^port=.*/port=$PORT/" /etc/my.cnf
@@ -42,3 +44,5 @@ sed -i "s/^bind-address.*/bind-address = 0.0.0.0/" /etc/my.cnf
 systemctl restart mysqld
 
 echo "MySQL 8.0 安装完成，端口为 $PORT，初始密码为 $PASSWORD，现在可以通过 0.0.0.0 访问。"
+
+mysql --connect-expired-password -u root -p";>WbMiDBt4:k" -e "alter user 'root'@'localhost' identified with mysql_native_password by 'Abcd1234@'";
